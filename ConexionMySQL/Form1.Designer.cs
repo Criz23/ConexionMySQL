4a17a6922a3317226494cbd294f32be2e53bfb08
Javier HP_Black W <j.rosales@alumnos.udg.mx>
2022-03-11 16:04:19 -0600

    Agregar archivos de proyecto.

---
+++ ConexionMySQL/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ConexionMySQL
+{
+    partial class ConexionSQL
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.checkBoxPwd = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbbGestor = new System.Windows.Forms.ComboBox();
+            this.txtDB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "User:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Pwd:";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(80, 51);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(296, 20);
+            this.txtHost.TabIndex = 4;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(80, 76);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(296, 20);
+            this.txtPort.TabIndex = 5;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(80, 126);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(296, 20);
+            this.txtUser.TabIndex = 6;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(80, 155);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(296, 20);
+            this.txtPwd.TabIndex = 7;
+            this.txtPwd.UseSystemPasswordChar = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(288, 193);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(88, 36);
+            this.btnConectar.TabIndex = 8;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // checkBoxPwd
+            // 
+            this.checkBoxPwd.AutoSize = true;
+            this.checkBoxPwd.Location = new System.Drawing.Point(80, 193);
+            this.checkBoxPwd.Name = "checkBoxPwd";
+            this.checkBoxPwd.Size = new System.Drawing.Size(117, 17);
+            this.checkBoxPwd.TabIndex = 9;
+            this.checkBoxPwd.Text = "Mostrar contraseña";
+            this.checkBoxPwd.UseVisualStyleBackColor = true;
+            this.checkBoxPwd.CheckedChanged += new System.EventHandler(this.checkBoxPwd_CheckedChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(38, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Gestor:";
+            // 
+            // cbbGestor
+            // 
+            this.cbbGestor.FormattingEnabled = true;
+            this.cbbGestor.Items.AddRange(new object[] {
+            "MySQL",
+            "PostgreSQL"});
+            this.cbbGestor.Location = new System.Drawing.Point(80, 21);
+            this.cbbGestor.Name = "cbbGestor";
+            this.cbbGestor.Size = new System.Drawing.Size(296, 21);
+            this.cbbGestor.TabIndex = 11;
+            // 
+            // txtDB
+            // 
+            this.txtDB.Location = new System.Drawing.Point(80, 101);
+            this.txtDB.Name = "txtDB";
+            this.txtDB.Size = new System.Drawing.Size(296, 20);
+            this.txtDB.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "DB:";
+            // 
+            // ConexionSQL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 259);
+            this.Controls.Add(this.txtDB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbbGestor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBoxPwd);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ConexionSQL";
+            this.Text = "Conexión SQL";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.CheckBox checkBoxPwd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbbGestor;
+        private System.Windows.Forms.TextBox txtDB;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ConexionMySQL/Form1.cs
@@ -0,0 +1,119 @@
+﻿/// <summary>
+/// Rosales Martínez Javier
+/// 210509386
+/// j.rosales@alumnos.udg.mx
+/// 05-03-22 10:00
+/// </summary>
+using System;
+using System.Windows.Forms;
+using MySqlConnector;
+using Npgsql;
+
+namespace ConexionMySQL
+{
+    /// <summary>
+    /// Creación del formulario Conexión SQL
+    /// </summary>
+    public partial class ConexionSQL : Form
+    {
+        #region Propiedades
+        private MySqlConnection conexionMySQL;
+        private NpgsqlConnection conexionPostgre;
+        #endregion
+
+        #region Constructor
+        public ConexionSQL()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Eventos
+        /// <summary>
+        /// Creación del evento click del botón conectar
+        /// </summary>
+        /// <param name="sender">object</param>
+        /// <param name="e">EventArgs</param>
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtHost.Text))
+            {
+                MessageBox.Show("El HOST es capmpo requerido");
+                return;
+            }
+            else if (string.IsNullOrEmpty(txtUser.Text))
+            {
+                MessageBox.Show("El USER es capmpo requerido");
+                return;
+            }
+            else if (string.IsNullOrEmpty(txtPwd.Text))
+            {
+                MessageBox.Show("La PWD es capmpo requerido");
+                return;
+            }
+            if (cbbGestor.Text == "MySQL")
+            {
+                try
+                {
+                    conexionMySQL = new MySqlConnection();
+                    conexionMySQL.ConnectionString = "server=" + txtHost.Text + ";" + "user id=" + txtUser.Text + ";" + "password=" + txtPwd.Text + ";";
+                    conexionMySQL.Open();
+                    MessageBox.Show("LA CONEXIÓN SE REALIZÓ CORRECTAMENTE");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("¡Hubo un error al conectar!: " + ex.Message);
+                }
+                finally
+                {
+                    if (conexionMySQL.State == System.Data.ConnectionState.Open)
+                        conexionMySQL.Close();
+                }
+            }
+            else 
+            {
+                if (string.IsNullOrEmpty(txtDB.Text))
+                {
+                    MessageBox.Show("La DB es capmpo requerido");
+                    return;
+                }
+                try
+                {
+                    conexionPostgre = new NpgsqlConnection();
+                    conexionPostgre.ConnectionString = "Server = " + txtHost.Text + ";" + "Port = " + txtPort.Text + ";" + "Database = " + txtDB.Text + ";" + "User Id=" + txtUser.Text + ";" + "Password=" + txtPwd.Text + ";";
+                    conexionPostgre.Open();
+                    MessageBox.Show("LA CONEXIÓN SE REALIZÓ CORRECTAMENTE");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("¡Hubo un error al conectar!: " + ex.Message);
+                }
+                finally
+                {
+                    if (conexionPostgre.State == System.Data.ConnectionState.Open)
+                        conexionPostgre.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creación del evento Checked del checkBoxPwd
+        /// </summary>
+        /// <param name="sender">object</param>
+        /// <param name="e">EventArgs</param>
+        private void checkBoxPwd_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPwd.Checked)
+            {
+                txtPwd.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPwd.UseSystemPasswordChar = true;
+            }
+        }
+        #endregion
+    }
+}
